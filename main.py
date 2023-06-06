37451027ff61b5925725752e376e0c010751accc
Viktor Oblomov <viktorall88@gmail.com.com>
2023-06-06 08:13:38 +0300

    rewrited to aiogram

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from dotenv import dotenv_values
 import openai
 import random
-import sqlite3
-import telebot
+import aiosqlite
+import hashlib
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, executor, types
 from requests.exceptions import ReadTimeout
 from openai.error import RateLimitError, InvalidRequestError
 
@@ -27,7 +29,9 @@ API_KEYS_CHATGPT = [
     env["API_KEY_CHATGPT_9"],
     env["API_KEY_CHATGPT_10"],
 ]
-bot = telebot.TeleBot(env["TG_BOT_TOKEN"])
+
+bot = Bot(token=env["TG_BOT_TOKEN"])
+dp = Dispatcher(bot)
 db_link = env["DB_LINK"]
 
 REKLAMA_MSG = [
@@ -37,16 +41,16 @@ REKLAMA_MSG = [
 ]
 
 
-def write_to_db(message):
-    conn = sqlite3.connect(db_link)
-    cursor = conn.cursor()
-    select_id = cursor.execute(
+async def write_to_db(message):
+    conn = await aiosqlite.connect(db_link)
+    cursor = await conn.cursor()
+    select_id = await cursor.execute(
         "SELECT id FROM user WHERE chat_id = ?", (str(message.chat.id),)
     )
-    select_id = select_id.fetchone()
+    select_id = await select_id.fetchone()
     if select_id:
         try:
-            cursor.execute(
+            await cursor.execute(
                 "UPDATE user SET last_msg=?, last_login=? WHERE chat_id=?",
                 (
                     message.text,
@@ -55,15 +59,15 @@ def write_to_db(message):
                 ),
             )
         except:
-            conn.commit()
-            conn.close()
-            bot.send_message(
+            await conn.commit()
+            await conn.close()
+            await bot.send_message(
                 612063160,
                 f"Ошибка при добавлении (INSERT) данных в базе Пользователь: {message.chat.id}",
             )
     else:
         try:
-            cursor.execute(
+            await cursor.execute(
                 "INSERT INTO user (chat_id, last_login, username, first_name, last_name, last_msg) VALUES (?,?,?,?,?,?)",
                 (
                     str(message.chat.id),
@@ -77,14 +81,14 @@ def write_to_db(message):
                 ),
             )
         except:
-            conn.commit()
-            conn.close()
-            bot.send_message(
+            await conn.commit()
+            await conn.close()
+            await bot.send_message(
                 612063160,
                 f"Ошибка при добавлении (INSERT) данных в базе Пользователь: {message.chat.id}",
             )
-    conn.commit()
-    conn.close()
+    await conn.commit()
+    await conn.close()
 
 
 def check_length(answer, list_of_answers):
@@ -96,72 +100,64 @@ def check_length(answer, list_of_answers):
         return list_of_answers
 
 
-def make_request(message, api_key_numb):
+async def make_request(message, api_key_numb, last_msg):
     chance = random.choices((0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+    await bot.send_chat_action(message.chat.id, "typing")
     try:
-        # engine = "text-davinci-003"
         engine = "gpt-3.5-turbo"
-        # engine = "gpt-4"
-        # completion = openai.Completion.create(
-        #     engine=engine,
-        #     prompt=message.text,
-        #     temperature=0.5,
-        #     max_tokens=4090,
-        # )
         completion = openai.ChatCompletion.create(
             model=engine, messages=[{"role": "user", "content": message.text}]
         )
-        # print(completion.choices[0]["message"])
         list_of_answers = check_length(
             completion.choices[0]["message"]["content"], []
         )
-
+        await bot.send_chat_action(message.chat.id, "typing")
         if list_of_answers:
             for piece_of_answer in list_of_answers:
-                bot.send_message(message.chat.id, piece_of_answer)
+                await bot.edit_message_text(
+                    chat_id=message.chat.id,
+                    message_id=last_msg.message_id,
+                    text=piece_of_answer,
+                )
             if chance == [1]:
-                bot.send_message(
-                    message.chat.id,
+                await message.answer(
                     random.choices(REKLAMA_MSG),
                     disable_web_page_preview=True,
                     parse_mode="HTML",
                 )
         else:
-            make_request(message, api_key_numb)
+            await make_request(message, api_key_numb, last_msg)
     except RateLimitError:
         if api_key_numb < len(API_KEYS_CHATGPT) - 1:
             api_key_numb += 1
             openai.api_key = API_KEYS_CHATGPT[api_key_numb]
-            make_request(message, api_key_numb)
+            await make_request(message, api_key_numb)
         else:
             if not key_end:
-                bot.send_message(
+                await bot.send_message(
                     612063160,
                     f"Ключи закончились!!!",
                 )
             key_end = True
-            bot.send_message(
-                message.chat.id,
+            await message.answer(
                 "ChatGPT в данный момент перегружен запросами, пожалуйста повторите свой запрос чуть позже.",
             )
     except ReadTimeout:
-        bot.send_message(
-            message.chat.id,
+        await message.answer(
             "ChatGPT в данный момент перегружен запросами, пожалуйста повторите свой запрос чуть позже.",
         )
     except InvalidRequestError:
-        bot.send_message(
-            message.chat.id,
+        await message.answer(
             "Максимальная длина контекста составляет около 3000 слов, ответ превысил длину контекста. Пожалуйста, повторите вопрос, либо перефразируйте его.",
         )
 
 
-def create_table():
+async def create_table():
     """Create table if not exists."""
 
-    conn = sqlite3.connect(db_link)
-    cursor = conn.cursor()
-    cursor.execute(
+    conn = await aiosqlite.connect(db_link)
+    cursor = await conn.cursor()
+    await cursor.executescript(
         """
         CREATE TABLE IF NOT EXISTS user(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -172,62 +168,127 @@ def create_table():
             last_name TEXT,
             last_msg TEXT
         );
+
+        CREATE TABLE IF NOT EXISTS donate(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            amount TEXT,
+            date TEXT,
+            FOREIGN KEY(user_id) REFERENCES user(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS premium(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            amount TEXT,
+            date TEXT,
+            FOREIGN KEY(user_id) REFERENCES user(id)
+        );
         """
     )
-    conn.commit()
-    conn.close()
+    await conn.commit()
+    await conn.close()
 
 
-@bot.message_handler(commands=["start"])
-def send_start(message):
+@dp.message_handler(commands=["start"])
+async def send_start(message: types.Message):
     text = """Приветствую ✌
 
 Я - ChatGPT, крупнейшая языковая модель, созданная OpenAI. 
 
-Я разработана для обработки естественного языка и могу помочь вам ответить на вопросы, 
-обсудить темы или предоставить информацию на различные темы.
+Я разработана для обработки естественного языка и могу помочь вам с ответами на ваши вопросы.
 
-🔥В том числе на русском языке....🔥
+Просто отправьте мне текстовое сообщение, и я постараюсь дать вам наилучший ответ.
 
-👇Я постараюсь ответить на твои вопросы👇
+Пожалуйста, имейте в виду, что я являюсь компьютерной программой и мои ответы не всегда могут быть точными или актуальными.
+
+Удачи! 🤖"""
+    await write_to_db(message)
+    await message.answer(text)
+
+
+@dp.message_handler(commands=["help"])
+async def send_start(message: types.Message):
+    text = """Приветствую ✌
+
+Введи запрос и я отвечу на него, постараюсь поддержать разговор, предоставить информацию.
+
+
 """
-    write_to_db(message)
-    bot.send_message(message.chat.id, text)
+    await write_to_db(message)
+    await message.answer(text)
 
 
-def check_key(message):
+def md5sign(m, oa, secretWord1, currency, o):
+    string = m + ":" + str(oa) + ":" + secretWord1 + ":" + currency + ":" + o
+    return hashlib.md5(string.encode("utf-8")).hexdigest()
+
+
+@dp.message_handler(commands=["donate"])
+async def send_donate(message: types.Message):
+    secretWord1 = "Jou^VC4buX_[1x?"
+    url = "https://pay.freekassa.ru/?"
+
+    # m - ID Вашего магазина merchantId
+    m = "32133"
+    # Сумма платежа
+    oa = "200"
+    # Валюта платежа
+    currency = "RUB"
+    # Номер заказа
+    o = str(message.message_id)
+    # Подпись
+    s = md5sign(m, oa, secretWord1, currency, o)
+    lang = "ru"
+    userid = str(message.from_user.id)
+    type = "donate"
+    params = f"m={m}&oa={oa}&currency={currency}&o={o}&s={s}&lang={lang}&userid={userid}&type={type}"
+
+    inline_btn_1 = types.InlineKeyboardButton(
+        text="Угостить ☕️", url=url + params
+    )
+    keyboard = types.InlineKeyboardMarkup(
+        row_width=1,
+    )
+    keyboard.add(inline_btn_1)
+    text = """Бот ChatGPT работает бесплатно, но Вы всегда можете угостить чашечкой кофе ☕️ разработчиков - глядишь чего полезного изобретут🙏
+
+ChatGPT bot is free, but you can always buy a cup of coffee ☕️ developers - see what they invent🙏
+    """
+    await message.reply(
+        text,
+        reply_markup=keyboard,
+    )
+
+
+async def check_key(message):
     key = message.text[19:]
     openai.api_key = key
     try:
-        # engine = "text-davinci-003"
-        # completion = openai.Completion.create(
-        #     engine=engine,
-        #     prompt=message.text,
-        #     temperature=0.5,
-        #     max_tokens=1000,
-        # )
         engine = "gpt-3.5-turbo"
         # engine = "gpt-4"
-        completion = openai.ChatCompletion.create(
+        await openai.ChatCompletion.create(
             model=engine, messages=[{"role": "user", "content": message.text}]
         )
-        bot.send_message(message.chat.id, f"Ключ {key} работает.")
+        await message.answer(f"Ключ {key} работает.")
     except:
-        bot.send_message(message.chat.id, f"Ключ {key} НЕ рабочий либо истек.")
+        await message.answer(f"Ключ {key} НЕ рабочий либо истек.")
 
 
-@bot.message_handler(content_types=["text"])
-def send_msg_to_chatgpt(message):
+@dp.message_handler(content_types=["text"])
+async def send_msg_to_chatgpt(message: types.Message):
     if CHECK_KEY == message.text[:19]:
         check_key(message)
         return
     api_key_numb = 0
     openai.api_key = random.choice(API_KEYS_CHATGPT)
-    write_to_db(message)
-    make_request(message, api_key_numb)
+    last_msg = await message.answer(
+        "<code>Сообщение принято. Ждем ответа...</code>", parse_mode="HTML"
+    )
+    await write_to_db(message)
+    await make_request(message, api_key_numb, last_msg)
 
 
 if __name__ == "__main__":
-    key_end = False
-    create_table()
-    target = bot.infinity_polling()
+    openai.api_key = API_KEYS_CHATGPT[0]
+    executor.start_polling(dp, skip_updates=True)
